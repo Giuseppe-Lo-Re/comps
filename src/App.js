29b58469f3b37080aec1da0ccfe747d928eade63
Giuseppe Lo Re <giuseppe.lore@dev4side.com>
2023-03-11 15:25:50 +0100

    Children props

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Button from "./button";
+
+function App() {
+    return (
+        <div>
+           <div>
+            <Button>Click here</Button>
+           </div>
+           <div>
+            <Button>Buy Now!</Button>
+           </div>
+           <div>
+            <Button>See Deal!</Button>
+           </div>
+           <div>
+            <Button>Hide Ads!</Button>
+           </div>
+           <div>
+            <Button>Something!</Button>
+           </div>
+           
+        </div>
+    )
+}
+
+export default App;
