fe2ea8a3ac1a5730a52c902ec09390a74423ddcc
Giuseppe Lo Re <giuseppe.lore@dev4side.com>
2023-03-12 11:09:02 +0100

    Created Accordion with conditional rendering

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -3,7 +3,7 @@ import Button from "../components/Button";
 
 function ButtonPage() {
     const handleClick = () => {
-        console.log('ClickS');
+        console.log('Click');
     };
 
     return (
