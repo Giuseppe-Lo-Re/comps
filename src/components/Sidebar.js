c135667244b1f65bf699113fb541edefa06eb840
Giuseppe Lo Re <giuseppe.lore@dev4side.com>
2023-03-13 18:31:43 +0100

    Added some styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import Link from "./link";
+import Link from "./Link";
 
 function Sidebar() {
     const links = [
@@ -8,11 +8,19 @@ function Sidebar() {
     ];
 
     const renderedLinks = links.map((link) => {
-        return <Link key={link.label} to={link.path}>{link.label}</Link>;
+        return (
+            <Link 
+                key={link.label} 
+                to={link.path} 
+                className='mb-3'
+                activeClassName="font-bold border-l-4 border-blue-500 pl-2"
+            >
+                {link.label}
+            </Link>);
     });
 
     return(
-        <div className="sticky top-0 overflow-y-scroll flex flex-col">
+        <div className="sticky top-0 flex flex-col items-start">
             {renderedLinks}
         </div>
     )
