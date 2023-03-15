4135b65c4f87d295381c18e82f0605b2b4084817
Giuseppe Lo Re <giuseppe.lore@dev4side.com>
2023-03-15 11:26:29 +0100

    Added icons and styling

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,4 +1,5 @@
 import Table from "./Table";
+import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go';
 import { useState } from "react";
 
 function SortableTable(props) {
@@ -27,9 +28,11 @@ function SortableTable(props) {
         return {
             ...column,
             header: () => (
-                <th onClick={() => handleClick(column.label)}>
-                    {getIcons(column.label, sortBy, sortOrder)}
-                    {column.label}
+                <th className="cursor-pointer hover:bg-gray-100" onClick={() => handleClick(column.label)}>
+                    <div className="flex items-center">
+                        {getIcons(column.label, sortBy, sortOrder)}
+                        {column.label}
+                    </div>
                 </th>
             ),
         };
@@ -62,15 +65,25 @@ function SortableTable(props) {
 
 function getIcons(label, sortBy, sortOrder) {
     if(label !== sortBy) {
-        return 'Show both icons';
+        return <div>
+            <GoArrowSmallUp />
+            <GoArrowSmallDown />
+        </div>;
     }
 
     if(sortOrder === null) {
-        return 'Show both icons';
+        return <div>
+            <GoArrowSmallUp />
+            <GoArrowSmallDown />
+        </div>;
     } else if(sortOrder === 'asc') {
-        return 'Show up icon';
+        return <div>
+            <GoArrowSmallUp />
+        </div>;
     } else if(sortOrder === 'desc') {
-        return 'Show down icon';
+        return <div>
+            <GoArrowSmallDown />
+        </div>;
     }
 }
 
