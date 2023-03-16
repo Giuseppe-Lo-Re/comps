faa3d50b17612a29e907edb3dd02ea52ad17f935
Giuseppe Lo Re <giuseppe.lore@dev4side.com>
2023-03-16 17:05:51 +0100

    trasformed counter in a custom hook

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,23 +1,15 @@
-import { useState, useEffect } from "react";
 import Button from "../components/Button";
+import useCounter from "../hooks/use-counter";
 
 function CounterPage({ initialCount }) {
-    const [count, setCount] = useState(initialCount);
+	const { count, increment } = useCounter(initialCount)
 
-    useEffect(() => {
-        console.log(count);
-    }, [count]);
-
-    const handleClick = () => {
-        setCount(count + 1);
-    };
-
-    return <div>
-        <h1>Count is {count}</h1>
-        <Button onClick={handleClick}>
-            Increment
-        </Button>
-    </div>
+	return (
+		<div>
+			<h1>Count is {count}</h1>
+			<Button onClick={increment}>Increment</Button>
+		</div>
+	)
 }
 
-export default CounterPage;
+export default CounterPage
